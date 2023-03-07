a145c8018b54c1a632e115b7635d202e51cc946e
WarriorQueen <natasha.bandopadhay@gmail.com>
2023-03-06 23:58:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+# Make the fruit name column the index column
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
 # Include a multi-select list so a user can pick fruits from the fruit list on the website
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
