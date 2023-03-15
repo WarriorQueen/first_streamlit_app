3d07d4fea82b9537c2621578dc3dbc5ae48999b6
WarriorQueen <natasha.bandopadhay@gmail.com>
2023-03-14 23:46:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Flatten the json data into a tabular format
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Insert flattened data into a dataframe. The website will show the data in a table.
+streamlit.dataframe(fruityvice_normalized)
